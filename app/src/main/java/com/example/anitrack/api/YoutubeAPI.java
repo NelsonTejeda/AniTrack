f0fa3c77b4d22e9829757e8fbf532e6096a59afb
NelsonTejeda <nelsonjesustejeda@gmail.com>
2021-03-29 20:04:18 -0400

    test

--- app/src/main/java/com/example/anitrack/api/YoutubeAPI.java
+++ app/src/main/java/com/example/anitrack/api/YoutubeAPI.java
@@ -2,6 +2,6 @@ package com.example.anitrack.api;
 
 public class YoutubeAPI {
 
-    public static final String YOUTUBE_API_KEY = "";
+    public static final String YOUTUBE_API_KEY = "AIzaSyCXuYStQzTcciEEgZ_DCA0F48agk6ar_xY";
     public YoutubeAPI(){}
 }
