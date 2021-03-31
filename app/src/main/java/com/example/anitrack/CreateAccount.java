31d5bc9e8bade4725c8468b2d7be40425c13ac1f
NelsonTejeda <nelsonjesustejeda@gmail.com>
2021-03-30 22:53:40 -0400

    user can login and logout

--- app/src/main/java/com/example/anitrack/CreateAccount.java
+++ app/src/main/java/com/example/anitrack/CreateAccount.java
@@ -3,14 +3,6 @@ package com.example.anitrack;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,61 +11,52 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    EditText emailEditText;
-    EditText passwordEditText;
-    Button signUpButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CreateAccount extends AppCompatActivity {
+    private static final String TAG = "CreateAccount";
     private FirebaseAuth mAuth;
-    private Context context;
-    private Button createAccount;
+    private EditText email;
+    private EditText password;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_create_account);
         mAuth = FirebaseAuth.getInstance();
-
-        emailEditText = findViewById(R.id.editTextTextEmailAddress);
-        passwordEditText = findViewById(R.id.editTextTextPassword);
-        createAccount = findViewById(R.id.newUserButton);
-        createAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToCreateScreen();
-            }
-        });
-
-        signUpButton = findViewById(R.id.buttonLogin);
-
+        email = findViewById(R.id.createAccountEmail);
+        password = findViewById(R.id.createAccountPassword);
+        signUpButton = findViewById(R.id.createAccountButtton);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-                createUser(email,password);
-
+                String e = email.getText().toString();
+                String p = password.getText().toString();
+                createUser(e,p);
             }
         });
-
     }
 
     private void createUser(String email, String password){
         if(!email.isEmpty() && !password.isEmpty()){
-            mAuth.signInWithEmailAndPassword(email, password)
+            mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
-                                Log.d(TAG, "signInWithEmail:success");
+                                Log.d(TAG, "createUserWithEmail:success");
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 updateUI(user);
                             } else {
                                 // If sign in fails, display a message to the user.
-                                Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(CreateAccount.this, "Authentication failed.",
                                         Toast.LENGTH_SHORT).show();
                                 updateUI(null);
                             }
@@ -97,11 +80,4 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         }
     }
-
-    public void goToCreateScreen(){
-        Intent i = new Intent(this,CreateAccount.class);
-        startActivity(i);
-    }
-
-
 }
